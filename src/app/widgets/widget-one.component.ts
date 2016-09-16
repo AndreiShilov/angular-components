c62913ec54f012bd7c050a4d6e4dfa7c60da632f
Andrei Shilov <andrei_shilov@epam.com>
2016-09-16 10:15:08 +0300

    Lesson [7] style components

--- src/app/widgets/widget-one.component.ts
+++ src/app/widgets/widget-one.component.ts
@@ -1,13 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
     selector: 'widget-one',
+    styles: [
+        `
+*{
+font-family: monospace;
+}
+:host{
+display: inline-block;
+border: 3px dashed black;
+color: green;
+}
+`
+    ],
     template: `<div *ngIf="selected"> one selected</div>`
 })
 export class WidgetOneComponent implements OnInit {
     selected = true;
-    constructor() { }
 
-    ngOnInit() { }
-    
+    constructor() {
+    }
+
+
+    ngOnInit() {
+    }
 }
